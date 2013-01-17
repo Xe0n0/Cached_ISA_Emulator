9b914c77813484130efc1d07a6bb77921397b19c
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-01-18 00:25:35 +0800

    define struct Cache

---
+++ cache.hh
@@ -0,0 +1,20 @@
+#pragma once
+
+struct Cache
+{
+	uint32_t n_ways;
+	uint32_t blk_size;
+	uint32_t n_sets;
+
+};
+
+struct Ret
+{
+	uint32_t set;
+	uint32_t line;
+	uint32_t addr_old;
+	uint32_t addr_new;
+};
+
+struct Cache* install_cache(uint32_t n_ways, uint32_t blk_size, uint32_t n_sets);
+bool access_test(struct Cache* cache, uint64_t addr, struct Ret* ret);
